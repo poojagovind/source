2c8a86782e923eff72e3ba7156ef236e02a84a3e
poojagovind <38188967+poojagovind@users.noreply.github.com>
2023-03-03 14:59:13 +0530

    Add files via upload

---
+++ Meeting rooms II (M).py
@@ -0,0 +1,18 @@
+def minMeetingRooms(intervals):
+    start = sorted([i[0] for i in intervals])
+    end = sorted([i[1] for i in intervals])
+    
+    res, count = 0, 0
+    i, j = 0, 0
+    
+    while i < len(intervals):
+        if start[i] < end[j]:
+            i += 1
+            count += 1
+        else:
+            j += 1
+            count -= 1
+            
+        res = max(res, count)
+        
+    return res
